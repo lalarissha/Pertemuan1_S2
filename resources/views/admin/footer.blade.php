b2f0fd52f928e13b5763375a68d37f7f1e5ddd9d
lalarissha <ulandaririssa16@gmail.com>
2024-07-11 21:36:16 +0700

    Pertemuan-11

---
+++ resources/views/admin/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+        <b>Praktikum Laravel 11</b>
+    </div>
+    <strong>Copyright &copy; Rissa Ulandari</a>.</strong> SI-04
+</footer>
